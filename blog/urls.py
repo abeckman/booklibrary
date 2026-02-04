5f54ccd9ba57bb67f1ab99bedceb8a0ea826684e
Arthur Beckman <aabeckman@yahoo.com>
2026-02-04 15:59:42 +0000

    cleaning up files which shouldn't be tracked

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path # path from polls and catalog apps
-from . import views # from polls and catalog apps
-#from django.urls import re_path # suggested replacement for orig url
-
-urlpatterns = [
-    path("", views.index, name='index'),
-    path("post/<slug>/", views.post_detail, name="blog-post-detail"),
-]
-
